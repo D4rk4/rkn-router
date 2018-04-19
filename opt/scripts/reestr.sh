e402a0a3ae34fc4418230fed664a9b72d6126af6
Dmitry <h31337@gmail.com>
2018-04-19 19:28:31 +0200

    switch to zapret-info data source

--- opt/scripts/reestr.sh
+++ opt/scripts/reestr.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 # agregate <> ftp://ftp.isc.org/isc/aggregate/aggregate-1.6.tar.gz
-source='https://reestr.rublacklist.net/api/ips'
+source='https://raw.githubusercontent.com/zapret-info/z-i/master/dump.csv'
 via=`netstat -rn | grep ^0.0.0.0| awk '{print $2}'`
 list='/etc/bird/rkn/reestr.conf'
 tmp='/tmp/rkn.ips'
 manual='/opt/scripts/static.cidr'
-vrfy='grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}''
 
-wget -qO- ${source}|tr \; '\n'| ${vrfy} | sort -n | uniq| sed 's/$/\/32/' > ${tmp}
+wget -qO- ${source}| cut -f 1 -d\; | tr '|' '\n' | sed 's/ //g' | sort -nr | uniq  | grep -v Updated | sed 's/\([0-9]\{1,3\}\.\)\{3\}[0-9]\{1,3\}$/&\/32/p' > ${tmp}
 cat ${manual} | sed '/^ *$/d' >> ${tmp}
 /opt/scripts/aggregate < /tmp/rkn.ips | sed "s/^/route /;s/$/ via ${via};/" > ${list} && service bird reload
